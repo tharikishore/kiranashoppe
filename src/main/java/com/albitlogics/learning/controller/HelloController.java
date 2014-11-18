d4bae2ae335a53a7526deae660ed28971cce4a7b
Harikishore Tadigotla <harikishore.t@gmail.com>
2014-11-18 22:37:05 +0530

    Initial version

---
+++ src/main/java/com/albitlogics/learning/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.albitlogics.learning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+@RequestMapping("/kirana")
+public class HelloController{
+ 
+   @RequestMapping(value="hello", method = RequestMethod.GET)
+   public String printHello(ModelMap model) {
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+      return "hello";
+   }
+
+}
